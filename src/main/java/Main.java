e0b98f5a2ce86cd654c98f9bab00d12a79b4d06a
YanaGv <gv.yanina@gmail.com>
2021-01-21 15:56:12 +0300

    Integer working Array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ import entity.Array;
 public class Main {
     public static void main(String[] args) {
         Array array = new Array(3);
+        System.out.println(array.getById(0));
     }
 }
--- src/main/java/entity/Array.java
+++ src/main/java/entity/Array.java
@@ -2,17 +2,27 @@ package entity;
 
 import java.awt.*;
 
-public class Array {
+public class Array{
     int size;
-    Object[] values;
+    Integer[] values;
 
     public Array() {
-        values = new Object[0];
+        values = new Integer[0];
         size = 0;
     }
-    public Array(int size) {
-        this.values = new Object[size];
-        this.size = size;
+    public Array(Integer firstNum) {
+        values = new Integer[1];
+        values[0] = firstNum;
+        size = 1;
     }
+    public Integer getById(int id){
+        try {
+            return values[id];
+        }catch (Exception ex){
+            System.out.println("Id overflow");
+        }
+        return 0;
+    }
+
 
 }
