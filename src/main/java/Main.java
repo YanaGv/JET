a5be5997653b61316a5c0ee9628858afcd00477b
YanaGv <gv.yanina@gmail.com>
2021-01-27 07:35:41 +0300

    main change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
-import entity.Array;
+import com.yanagv.task.array.entity.CustomArray;
+import com.yanagv.task.array.exeption.CustomArrayException;
 
 public class Main {
     public static void main(String[] args) {
-        Array array = new Array(3);
-        System.out.println(array.getById(0));
+        int[] array = {1, 2, 3};
+        CustomArray carray;
+        carray = new CustomArray(array);
+        System.out.println(carray.getElement(0));
     }
 }
