3331697287bb6722fb45ef18fe5e40ba51befe3b
YanaGv <gv.yanina@gmail.com>
2021-01-27 07:34:50 +0300

    array fix added

--- src/main/java/com/yanagv/task/array/entity/CustomArray.java
+++ src/main/java/com/yanagv/task/array/entity/CustomArray.java
@@ -2,7 +2,9 @@ package com.yanagv.task.array.entity;
 
 import com.yanagv.task.array.exeption.CustomArrayException;
 
-public class CustomArray implements Comparable<CustomArray>{
+import java.nio.file.Files;
+
+public class CustomArray implements Comparable<CustomArray> {
     int[] array;
 
     public CustomArray() {
@@ -12,13 +14,13 @@ public class CustomArray implements Comparable<CustomArray>{
     public CustomArray(int... args) {
         int size = args.length;
         this.array = new int[size];
-        for (int i : args) {
+        for (int i = 0; i < args.length; i++) {
             this.array[i] = args[i];
         }
     }
 
     public CustomArray(int size) throws CustomArrayException {
-        if (size >= 0){
+        if (size >= 0) {
             this.array = new int[0];
         } else {
             throw new CustomArrayException("Negative size");
@@ -29,16 +31,19 @@ public class CustomArray implements Comparable<CustomArray>{
     public int getElement(int index) {
         return this.array[index];
     }
-    public void setElement(int index, int value){
+
+    public void setElement(int index, int value) {
         this.array[index] = value;
     }
+
     public int length() {
         return this.array.length;
     }
-    public String toString(){
+
+    public String toString() {
         String label = "[";
         StringBuilder stringBuilder = new StringBuilder();
-        for(int element: this.array){
+        for (int element : this.array) {
             stringBuilder.append(element + " ");
         }
         label += stringBuilder.toString();
@@ -49,6 +54,20 @@ public class CustomArray implements Comparable<CustomArray>{
     @Override
     public int compareTo(CustomArray comparableArray) {
         return this.array.length - comparableArray.length();
+    }
+
+    public boolean equals(CustomArray secondArray) {
+        if (this.compareTo(secondArray) == 0) {
+            int i = 0;
+            while (i < this.array.length) {
+                if (this.array[i] != secondArray.getElement(i)) {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+        return false;
     }
 
 }
